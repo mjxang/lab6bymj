31114d4bd2a3354b9d82e668b4d4fd4f9aec79a6
MJ Section D <n01615449@humber.ca>
2024-06-13 13:16:38 -0400

    task 2 done + comments

---
+++ task2.js
@@ -0,0 +1,11 @@
+//Task 2: Exploring Loops
+
+//Loop to iterate 1-10
+for (let i=0; i<=10; i++){
+    console.log(i);
+}
+
+//Calculating and printing loop for first 5 multiples of 3
+for (let i=1; i <=5; i++){
+    console.log(3*i);
+}
