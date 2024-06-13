0b274fcd5b07f8909e9e70f624a554fe8af97e33
MJ Section D <n01615449@humber.ca>
2024-06-13 13:10:21 -0400

    task 1 done + comments

---
+++ task1.js
@@ -0,0 +1,8 @@
+//Task 1: Working with Variables
+
+//declaring and assigning the variables
+let myNumber = 10;
+let myString = "Hello, JavaScript!";
+//printing the variables
+console.log(myNumber);
+console.log(myString);
