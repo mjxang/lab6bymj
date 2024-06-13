343ab95c776908e05ed2289a29b86d9a9a1d37e2
MJ Section D <n01615449@humber.ca>
2024-06-13 13:28:06 -0400

    formatted task 2 and task 3
    using vs extension "prettier" lol

--- task3.js
+++ task3.js
@@ -1,17 +1,17 @@
 //Task 3: Discovering Functions
 
 //function that adds two numbers
-function addNumbers(num1, num2){
-    return num1 + num2;
+function addNumbers(num1, num2) {
+  return num1 + num2;
 }
 
 //call add function and print result
-console.log(addNumbers(4,7));
+console.log(addNumbers(4, 7));
 
 //multiply function for two numbers
-function multiplyNumbers(num1,num2){
-    return num1*num2;
+function multiplyNumbers(num1, num2) {
+  return num1 * num2;
 }
 
 //call multiplyNumbers and print result
-console.log(multiplyNumbers(4,5));
+console.log(multiplyNumbers(4, 5));
